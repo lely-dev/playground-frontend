b621686b1dfde525fbeb8d193561d077f3ab0367
elisa carboni <elisacarboni@live.com>
2024-06-12 13:25:17 +0200

    registration page

--- src/Components/Page/Register/PlayerRegistration.jsx
+++ src/Components/Page/Register/PlayerRegistration.jsx
@@ -96,8 +96,8 @@ export default function PlayerRegistration() {
 
   return (
     <>
-      <section className="h-screen">
-        <div className="h-full">
+      <section>
+        <div>
           {/* <!-- Left column container with background--> */}
           <div className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
             <div className="shrink-1 mb-12 grow-0 basis-auto md:mb-0 md:w-9/12 md:shrink-0 lg:w-6/12 xl:w-6/12">
@@ -109,7 +109,7 @@ export default function PlayerRegistration() {
             </div>
 
             {/* <!-- Right column container --> */}
-            <div className="mb-12 md:mb-10 md:w-8/12 lg:m-10 lg:w-5/12 xl:w-5/12">
+            <div className="mb-12 md:mb-10 md:w-8/12 lg:m-10 lg:w-5/12 xl:w-5/12 flex justify-center">
               <form>
                 {/* <!--Sign in section--> */}
                 <div className="flex flex-row items-center justify-center lg:justify-start">
@@ -200,21 +200,22 @@ export default function PlayerRegistration() {
                     OR
                   </p>
                 </div>
+                <div className="mb-30 flex flex-col items-center gap-5">
+                  {/* <!-- Social login buttons --> */}
+                  <LoginGoogle />
 
-                {/* <!-- Social login buttons --> */}
-                <LoginGoogle />
-
-                {/* <!-- Login button --> */}
-                <div className="text-center lg:text-left">
-                  <TERipple rippleColor="light">
-                    <button
-                      type="button"
-                      className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                      onClick={registerPlayer}
-                    >
-                      Register
-                    </button>
-                  </TERipple>
+                  {/* <!-- Login button --> */}
+                  <div className="text-center lg:text-left">
+                    <TERipple rippleColor="light">
+                      <button
+                        type="button"
+                        className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                        onClick={registerPlayer}
+                      >
+                        Register
+                      </button>
+                    </TERipple>
+                  </div>
                 </div>
               </form>
             </div>
